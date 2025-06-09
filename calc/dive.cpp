5eabcf3eaee120809217467ea4a96989189d8971
202207044 <wjdalstjr8236@gmail.com>
2025-06-09 10:16:49 +0900

    정민석 dive.cpp 3차 수정

--- calc/dive.cpp
+++ calc/dive.cpp
@@ -1,11 +1,10 @@
 #include "basic.h"
-  3 int dive(int __x, int  __y)
-  4 {
-  5     int cnt = 0;
-  6     while (__x >= __y) {
-  7         __x = sub(__x, __y);
-  8         cnt++;
-  9     }
- 10     return cnt;
- 11 }
-
+ int dive(int __x, int  __y)
+ {
+     int cnt = 0;
+     while (__x >= __y) {
+         __x = sub(__x, __y);
+         cnt++;
+     }
+     return cnt;
+ }
